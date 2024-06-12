0aa21b20b7184912c4c84db2c60c64b5151ec3a9
SeeNoirSeeNo <besttechieseu@gmail.com>
2024-06-12 21:50:17 +0200

    works now?

--- MainScenes/Battle.gd
+++ MainScenes/Battle.gd
@@ -21,31 +21,9 @@ func _ready():
 	unit_factory.create_unit("FlyingEye", Vector2i(15,3), "Enemy", "Enemy", true)
 	unit_factory.create_unit("FlyingEye", Vector2i(17,3), "Enemy", "Enemy", true)
 
-#SPAWN RANDOM UNITS
-#	for i in range(1): 
-#		var random_cell = navigation_grid.get_free_random_cell_local()
-#		unit_factory.create_unit("CaveGoblin", random_cell, "Enemy", "Enemy")
-#		random_cell = navigation_grid.get_free_random_cell_local()
-#		unit_factory.create_unit("FlyingEye", random_cell, "Enemy", "Enemy")
-
-
-func _on_unit_created(unit):
-	unit.turn_finished.connect(timeline._on_turn_finished)
-
-
-
-	for i in range(5): 
-		var random_cell = navigation_grid.get_free_cell_local()
-		unit_factory.create_unit("CaveGoblin", random_cell, "Enemy", "Enemy")
-		random_cell = navigation_grid.get_free_cell_local()
-		unit_factory.create_unit("FlyingEye", random_cell, "Enemy", "Enemy")
-		print(navigation_grid.tile_map.local_to_map(random_cell))
-
-	
-
 func connect_signals():
 		map_picker.map_loaded.connect(navigation_grid._on_map_loaded)
 		map_picker.map_loaded.connect(unit_factory._on_map_loaded)
 		unit_factory.unit_created.connect(navigation_grid._on_unit_created)
 		unit_factory.unit_created.connect(timeline._on_unit_created)
-		unit_factory.unit_created.connect(self._on_unit_created)
+		#unit_factory.unit_created.connect(self._on_unit_created)
